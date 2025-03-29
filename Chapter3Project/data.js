73b284a0e9efd52281870476a9b8853165d0bb7c
Chyenne2003 <ChyenneDickens840@gmail.com>
2025-03-29 13:42:33 -0500

    Add files via upload

---
+++ Chapter3Project/data.js
@@ -0,0 +1,23 @@
+// Chyenne Dickens, 03/29/2025
+
+let fullName = "Chyenne Dickens";
+console.log("Full Name:", fullName);
+
+let desiredAnnualSalary = 100000;
+console.log("Desired Annual Salary:", desiredAnnualSalary);
+
+let isVeteran = false;
+console.log("Veteran Status:", isVeteran);
+
+let friends = ["Ruth", "Chris", "Nicki"];
+console.log("Friends:", friends);
+
+let friendsSalaries = [80000, 150000, 72000];
+console.log("Friends' Desired Salaries:", friendsSalaries);
+
+let anotherFriend = {
+    firstName: "Jada",
+    lastName: "Williams",
+    desiredSalary: 90000
+};
+console.log("Another Friend:", anotherFriend);
